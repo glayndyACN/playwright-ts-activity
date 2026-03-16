3eb5274ef374302f594465eb18a41f5537784334
Caltheia <84892823+Caltheia@users.noreply.github.com>
2026-03-16 14:50:09 +0800

    3.16

--- tests/bea.spec.ts
+++ tests/bea.spec.ts
@@ -3,10 +3,11 @@ import { test, expect } from '@playwright/test';
 test.only('Playwright Activity - Bea', async ({ page }) => {
   await page.goto('http://localhost:4200/');
 
+
   await page.locator('input[placeholder="Jane Doe"]').fill('Jane Doe');
   await page.locator('button.appearance-filled').click();
 
-  
+
   const forms = page.locator('form');
 
   const usingTheGridForm = forms.nth(1);
@@ -21,7 +22,7 @@ test.only('Playwright Activity - Bea', async ({ page }) => {
 
   await forms.nth(4).getByText('Send').click();
 
-
+  
   await usingTheGridForm.locator('input[type="radio"]').nth(1).check();
   await forms.nth(5).getByRole('button', { name: 'Sign in' }).click();
 
@@ -30,7 +31,7 @@ test.only('Playwright Activity - Bea', async ({ page }) => {
   await basicFormCard.locator('input').nth(1).fill('Pass1234');
   await basicFormCard.getByRole('button', { name: 'Submit' }).click();
 
- 
+  // 5. Reuse Locators
   const emailField = basicFormCard.locator('input').nth(0);
   const passwordField = basicFormCard.locator('input').nth(1);
   const submitButton = basicFormCard.getByRole('button', { name: 'Submit' });
